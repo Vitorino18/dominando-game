2113ccc1a528972c70fb0066e3bd00fc276744cf
vitorjq <vitor.joaquimcs@gmail.com>
2026-09-11 16:58:08 -0300

    feat: estrutura inicial criada do jogo

--- src/main/java/br/com/dominando/Main.java
+++ src/main/java/br/com/dominando/Main.java
@@ -1,16 +1,31 @@
-package br.com.dominando;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+package br.com.dominando;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    @Override
+    public void start (Stage stage){
+        // texto que pode ser criado //
+        Label titulo = new Label("dominado");
+
+        StackPane raiz = new StackPane();
+        raiz.getChildren().add(titulo);
+
+        // seta como pode ser a tela e o que pode ser colocada nela //
+        Scene cena = new Scene(raiz,1000, 800);
+
+        // cria a tela e o que vai ser impressa nela. //
+        stage.setTitle("jogo");
+        stage.setScene(cena);
+        stage.show();
         }
+    // inicia o aplicativo //
+    public static void main(String[] args) {
+        launch();
     }
 }
